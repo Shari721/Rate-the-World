237a65841b7ec5619f6be469273a8c3f2092c212
Nick Volkert <nick@nickvolkert.com>
2018-05-02 18:57:28 -0500

    works on yarn start now

--- rate-world/src/App.js
+++ rate-world/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Footer from './components/Footer';
 import Card from './components/Card';
@@ -10,15 +9,12 @@ import Jumbotron from './components/Jumbotron';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Wrapper>
+        <Navbar />
+        <Jumbotron />
+        <Card />
+        <Footer />
+      </Wrapper>
     );
   }
 }
